57f48e16f35b44a20dfb30d7c35f1e94628c4f84
Greg Sharpe <me@gregsharpe.co.uk>
2018-12-04 16:59:26 +0000

    print return code

--- add-comment.py
+++ add-comment.py
@@ -48,8 +48,8 @@ def get_remove_old_comment():
             DELETE_COMMENT_URL = COMMENTS_GITHUB_URI + "/{}".format(comment["id"])
             print DELETE_COMMENT_URL
             response = requests.delete(DELETE_COMMENT_URL, headers=API_HEADER)
-            print response
-            
+            print response.json()
+
     return response.json()
 
 
